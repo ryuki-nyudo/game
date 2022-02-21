f03e4a7fbe663cc4374ed2e8a8e81aed549124df
megamoli <morihiro-oosiro-20h@stu.kbc.ac.jp>
2022-02-21 14:42:04 +0900

    プレイヤーの左右移動とカメラの追従移動の追加

---
+++ Gametaisyou/Assets/Script/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // 物理演算をしたい場合はFixedUpdateを使うのが一般的
+    void FixedUpdate()
+    {
+        float horizontalKey = Input.GetAxis("Horizontal");
+        float VerticalKey = Input.GetAxis("Vertical");
+
+        //右入力で左向きに動く
+        if (horizontalKey > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        //左入力で左向きに動く
+        else if (horizontalKey < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        
+        //上入力で上向きに動く
+        if (VerticalKey > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.x);
+        }
+        //下入力で下向きに動く
+        else if (VerticalKey < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.x);
+        }
+
+        ////ボタンを離すと止まる
+        //else
+        //{
+        //    rb.velocity = Vector2.zero;
+        //}
+    }
+}
