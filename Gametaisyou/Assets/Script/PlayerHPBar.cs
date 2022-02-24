086b837be63d4d6c3788fca06a4cf01e3122dc89
KBC12006040\kbc12006040 <ryuki-nyudou-20h@stu.kbc.ac.jp>
2022-02-24 10:08:29 +0900

    当たり判定

---
+++ Gametaisyou/Assets/Script/PlayerHPBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPBar : MonoBehaviour
+{
+    //最大HPと現在のHP。
+    int maxHp = 100;
+    int currentHp;
+
+    public Slider slider;
+
+    void Start(){
+        slider.value = 1;
+        //現在のHPを最大HPと同じに。
+        currentHp = maxHp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider){
+        //Enemyタグのオブジェクトに触れると発動
+        if (collider.gameObject.tag == "enemy"){
+            int damage = 10;
+
+            //現在のHPからダメージを引く
+            currentHp = currentHp - damage;
+
+            //最大HPにおける現在のHPをSliderに反映。
+            //int同士の割り算は小数点以下は0になるので、
+            //(float)をつけてfloatの変数として振舞わせる。
+            slider.value = (float)currentHp / (float)maxHp; ;
+        }
+    }
+}
