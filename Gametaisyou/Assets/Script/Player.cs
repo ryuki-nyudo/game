042451021bcccf4dbf6f7e3c975b40bdf3e730ef
HikaruTakaoka <hikaru-takaoka-20h@stu.kbc.ac.jp>
2022-03-01 14:44:13 +0900

    スタミナ、酸素追加

--- Gametaisyou/Assets/Script/Player.cs
+++ Gametaisyou/Assets/Script/Player.cs
@@ -11,15 +11,29 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb;
     public bool pflag;
+    public bool slow;
 
-    void Start(){
+    bool tap = true;
+    int maxSt = 100;
+    double currentSt;
+
+    public Slider slider;
+
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
 
         pflag = false;
+        slow = false;
+
+        slider.value = 1;
+        currentSt = maxSt;
+
     }
 
     // 物理演算をしたい場合はFixedUpdateを使うのが一般的
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         float horizontalKey = Input.GetAxis("Horizontal");
         float VerticalKey = Input.GetAxis("Vertical");
 
@@ -37,8 +51,9 @@ public class Player : MonoBehaviour
         }
 
         //上入力で上向きに動く
-        if (VerticalKey > 0){
-            rb.velocity = new Vector2(rb.velocity.x,speed);
+        if (VerticalKey > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed);
 
             Accel();
 
@@ -51,6 +66,26 @@ public class Player : MonoBehaviour
             Accel();
 
         }
+        if (currentSt < maxSt)
+        {
+            currentSt += 0.09;
+            slider.value = (float)currentSt / (float)maxSt; ;
+            if (slow == true)
+            {
+                speed = 1;
+                currentSt += 0.35;
+                slider.value = (float)currentSt / (float)maxSt; ;
+                if (currentSt > 70)
+                {
+                    slow = false;
+                    speed = 3;
+                }
+            }
+        }
+        if (currentSt < 10)
+        {
+            slow = true;
+        }
 
     }
 
@@ -58,14 +93,31 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            speed += 15;
-            pflag = true;
-            Invoke("Decelerate", 0.3f);
+            if (tap == true)
+            {
+                if (slow == false)
+                {
+                    if (currentSt >= 10)
+                    {
+                        int move = 10;
+
+                        currentSt = currentSt - move;
+
+                        slider.value = (float)currentSt / (float)maxSt; ;
+                        tap = false;
+                        speed += 15;
+                        pflag = true;
+                        Invoke("Decelerate", 0.3f);
+                    }
+                }
+            }
         }
     }
 
-    void Decelerate(){
+    void Decelerate()
+    {
         speed -= 15;
         pflag = false;
+        tap = true;
     }
 }
