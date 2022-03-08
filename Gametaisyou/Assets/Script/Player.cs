9c097d6961e8996a68f8c61e88126189d401a5a5
megamoli <morihiro-oosiro-20h@stu.kbc.ac.jp>
2022-03-08 14:19:51 +0900

    no message

--- Gametaisyou/Assets/Script/Player.cs
+++ Gametaisyou/Assets/Script/Player.cs
@@ -34,7 +34,7 @@ public class Player : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
 
-        iflag = false;
+        //iflag = false;
 
         pflag = false;
         slow = false;
