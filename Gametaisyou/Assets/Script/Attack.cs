e12dc3c9889a7048eb17d8d321a5811e5582a37f
KBC12006040\kbc12006040 <ryuki-nyudou-20h@stu.kbc.ac.jp>
2022-03-08 13:42:03 +0900

    item自動未完成

--- Gametaisyou/Assets/Script/Attack.cs
+++ Gametaisyou/Assets/Script/Attack.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Attack : MonoBehaviour{
     GameObject Player;
     Player player;
+    Vector2 position;
 
-    GameObject Kai;
+    //ポジション取得
+    public GameObject HPitem;
+    public GameObject MPitem;
 
     void Start(){
         Player = GameObject.Find("player");
         player = Player.GetComponent<Player>();
-        Kai = GameObject.Find("Kai");
-        Kai.gameObject.SetActive(false);
     }
-    //Update is called once per frame
+
+    //茶色木箱に当たったときの動き
     void OnCollisionEnter2D(Collision2D other){
         if(other.gameObject.tag == "box" && player.pflag == true){
-            other.gameObject.SetActive(false);
-            Kai.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            position = other.gameObject.transform.position;
+            Instantiate(HPitem,position,Quaternion.identity);
+            HPitem.gameObject.SetActive(true);
+        }
+
+        if(other.gameObject.tag == "boxb" && player.pflag == true){
+            Destroy(other.gameObject);
+            position = other.gameObject.transform.position;
+            Instantiate(MPitem,position,Quaternion.identity);
+            MPitem.gameObject.SetActive(true);
         }
     }
 }
