110d71b77878d99c1a7a28b981d99d454538e66c
KBC12006034\kbc12006034 <moriki-oota-20h@stu.kbc.ac.jp>
2022-03-15 09:23:57 +0900

    no message

---
+++ Gametaisyou/Assets/Script/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    private float speed;
+
+    void Start()
+    {
+        speed = 5;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        rb2d.velocity = new Vector2(input * speed, rb2d.velocity.y);
+
+        //進行方向へ向きを変える
+        if (input < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        else if (input > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+    }
+}
