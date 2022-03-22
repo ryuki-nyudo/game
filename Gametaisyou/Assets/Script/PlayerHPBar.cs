85f8c6a0aab9920124a700cc69e87ba3452d6ac9
KBC12006040\kbc12006040 <ryuki-nyudou-20h@stu.kbc.ac.jp>
2022-03-22 09:20:28 +0900

    Stage完成

--- Gametaisyou/Assets/Script/PlayerHPBar.cs
+++ Gametaisyou/Assets/Script/PlayerHPBar.cs
@@ -15,12 +15,17 @@ public class PlayerHPBar : MonoBehaviour
 
     GameObject Player;
     Player player;
+    GameObject Attack;
+    Attack attack;
 
     public float pTime;
     public float timer = 0.5f;
 
-    // AudioSource audioSource;
-    // public AudioClip HPcaveat;
+    AudioSource audioSource;
+    public AudioClip HPcaveat;
+    bool hflag;
+    public float audio;
+    public float htimer = 5.0f;
     void Start(){
         slider.value = 1;
         //現在のHPを最大HPと同じに。
@@ -28,14 +33,23 @@ public class PlayerHPBar : MonoBehaviour
 
         Player = GameObject.Find("player");
         player = Player.GetComponent<Player>();
+
+        audioSource = GetComponent<AudioSource>();
+        hflag = false;
     }
 
     void Update(){
         pTime += Time.deltaTime;
+        audio += Time.deltaTime;
 
-        // if(currentHp <= 75){
-        //     audioSource.PlayOneShot(HPcaveat);
-        // }
+        if(currentHp <= 75){
+            hflag = true;
+            if(hflag == true && audio >= htimer){
+                audioSource.PlayOneShot(HPcaveat);
+                audio = 0f;
+                hflag = false;
+            }
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D other){
@@ -54,14 +68,13 @@ public class PlayerHPBar : MonoBehaviour
             Destroy(other.gameObject);
         }
 
-        if(other.gameObject.tag == "box" || other.gameObject.tag == "boxb"){
+        if(other.gameObject.tag == "itembox" || other.gameObject.tag == "boxb"){
             pTime = 0f;
         }
     }
 
     public void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "HPitem" && timer <= pTime){
-            other.gameObject.SetActive(false);
             currentHp = currentHp + recovery;
             slider.value = (float)currentHp / (float)maxHp;
         }
--- Gametaisyou/Assets/Script/itemScript.cs
+++ Gametaisyou/Assets/Script/itemScript.cs
@@ -1,50 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class itemScript : MonoBehaviour
-{
-    GameObject Player;
-    Player player;
-    GameObject Attack;
-    Attack attack;
+// public class itemScript : MonoBehaviour
+// {
+//     GameObject Attack;
+//     Attack attack;
 
-    public float iTime;
-    public float timer = 0.5f;
-    public float jTime;
+//     public float iTime;
+//     public float timer = 0.5f;
+//     public float jTime;
 
-    public bool iflag = false;
+//     public bool iflag = false;
 
-    void Start(){
-        Player = GameObject.Find("player");
-        player = Player.GetComponent<Player>();
-        Attack = GameObject.Find("player");
-        attack = Attack.GetComponent<Attack>();
+//     void Start(){
+//         Attack = GameObject.Find("player");
+//         attack = Attack.GetComponent<Attack>();
 
-        //最初は見えなくする
-        gameObject.SetActive(false);
-    }
+//         gameObject.SetActive(false);
+//     }
 
-    void Update (){
-        iTime += Time.deltaTime;
-        if(iflag == true && jTime <= 3f){
-            jTime += Time.deltaTime;
-        }
-        else if(iflag == true && jTime > 3f){
-            iflag = false;
-        }
-    }
+//     void FixedUpdate (){
+//         iTime += Time.deltaTime;
+//         if(iflag == true && jTime <= 3f){
+//             jTime += Time.deltaTime;
+//         }
+//         else if(iflag == true && jTime > 3f){
+//             iflag = false; 
+//         }
+        
+//         //item処理
+//         for(;attack.HPflag == true;){
+//             iTime = 0f;
+//             attack.HPflag = false;
+//             gameObject.SetActive(true);
+//         }
+//     }
 
-    public void OnCollisionEnter2D(Collision2D player){
-        if(player.gameObject.tag == "box"|| player.gameObject.tag == "boxb"){
-            iTime = 0f;
-        }
-    }
-
-    public void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.tag == "player" && timer <= iTime){
-            iflag = true;
-            jTime = 0f;
-        }
-    }
-}
+//     public void OnCollisionEnter2D(Collision2D other){
+//         if(other.gameObject.tag == "Player" && timer <= iTime){
+//             Debug.Log("unti");
+//             gameObject.SetActive(false);
+//         }
+//     }
+// }
