346d6dcb2ba3e2b7e0f3f26620b941c43f2044ea
MSI\kbc12006174 <yogi-sayato-20h@stu.kbc.ac.jp>
2022-03-29 09:26:09 +0900

    Merge branch 'yogi'
    # Conflicts:
    #	Gametaisyou/Library/ArtifactDB
    #	Gametaisyou/Library/CurrentLayout-default.dwlt
    #	Gametaisyou/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	Gametaisyou/Library/SourceAssetDB
    #	Gametaisyou/Library/StateCache/SceneView/1f69c5-mainStage.json
    #	Gametaisyou/obj/Debug/Assembly-CSharp.csprojAssemblyReference.cache
    #	Gametaisyou/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

--- Gametaisyou/Assets/Script/itemScript.cs
+++ Gametaisyou/Assets/Script/itemScript.cs
@@ -1,46 +1,53 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-// public class itemScript : MonoBehaviour
-// {
-//     GameObject Attack;
-//     Attack attack;
+//public class itemScript : MonoBehaviour
+//{
+//    GameObject Attack;
+//    Attack attack;
 
-//     public float iTime;
-//     public float timer = 0.5f;
-//     public float jTime;
+//    public float iTime;
+//    public float timer = 0.5f;
+//    public float jTime;
 
-//     public bool iflag = false;
+//    public bool iflag = false;
 
-//     void Start(){
-//         Attack = GameObject.Find("player");
-//         attack = Attack.GetComponent<Attack>();
+//    void Start()
+//    {
+//        Attack = GameObject.Find("player");
+//        attack = Attack.GetComponent<Attack>();
 
-//         gameObject.SetActive(false);
-//     }
+//        gameObject.SetActive(false);
+//    }
 
-//     void FixedUpdate (){
-//         iTime += Time.deltaTime;
-//         if(iflag == true && jTime <= 3f){
-//             jTime += Time.deltaTime;
-//         }
-//         else if(iflag == true && jTime > 3f){
-//             iflag = false; 
-//         }
-        
-//         //item処理
-//         for(;attack.HPflag == true;){
-//             iTime = 0f;
-//             attack.HPflag = false;
-//             gameObject.SetActive(true);
-//         }
-//     }
+//    void FixedUpdate()
+//    {
+//        iTime += Time.deltaTime;
+//        if (iflag == true && jTime <= 3f)
+//        {
+//            jTime += Time.deltaTime;
+//        }
+//        else if (iflag == true && jTime > 3f)
+//        {
+//            iflag = false;
+//        }
 
-//     public void OnCollisionEnter2D(Collision2D other){
-//         if(other.gameObject.tag == "Player" && timer <= iTime){
-//             Debug.Log("unti");
-//             gameObject.SetActive(false);
-//         }
-//     }
-// }
+//        //item処理
+//        for (; attack.HPflag == true;)
+//        {
+//            iTime = 0f;
+//            attack.HPflag = false;
+//            gameObject.SetActive(true);
+//        }
+//    }
+
+//    public void OnCollisionEnter2D(Collision2D other)
+//    {
+//        if (other.gameObject.tag == "Player" && timer <= iTime)
+//        {
+//            Debug.Log("unti");
+//            gameObject.SetActive(false);
+//        }
+//    }
+//}
