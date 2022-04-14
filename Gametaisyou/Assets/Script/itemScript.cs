562010c82da7103aff796d432b0e321da9896287
megamoli <morihiro-oosiro-20h@stu.kbc.ac.jp>
2022-04-14 15:22:17 +0900

    no message

--- Gametaisyou/Assets/Script/itemScript.cs
+++ Gametaisyou/Assets/Script/itemScript.cs
@@ -19,16 +19,21 @@ public class itemScript : MonoBehaviour
     public bool ixflag;
 
     BoxDestroy script;
+    GameObject woodbox;
 
     void Start(){
         Attack = GameObject.Find("player");
         attack = Attack.GetComponent<Attack>();
 
         item.gameObject.SetActive(false);
-        Box.gameObject.SetActive(true);
+        //Box.gameObject.SetActive(true);
         ixflag = true;
-    }
-    
+
+        woodbox = GameObject.FindWithTag("itembox");
+        script = woodbox.GetComponent<BoxDestroy>();
+
+            }
+
     //    void Update(){
     //        //item処理
     //        if (Box.activeSelf == false && ixflag == true)
@@ -40,15 +45,17 @@ public class itemScript : MonoBehaviour
     //        }
     //    }
     //}
-    void Update()
-    {
+    void OnCollisionEnter2D(Collision2D collision) {
         //item処理
-        if ( script.col.enabled == false && ixflag == true)
-        {
-            iTime = 0f;
-            ixflag = false;
-            attack.HPflag = false;
-            item.gameObject.SetActive(true);
+        if (collision.gameObject.tag == "Player") {
+            if (script.KAIsyutugen == true && ixflag == true)
+            {
+
+                iTime = 0f;
+                ixflag = false;
+                attack.HPflag = false;
+                item.gameObject.SetActive(true);
+            }
         }
     }
 }
