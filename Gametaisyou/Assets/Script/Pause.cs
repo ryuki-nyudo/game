5ea055da9b1853baaf41765687dc9abb729613d7
HikaruTakaoka <hikaru-takaoka-20h@stu.kbc.ac.jp>
2022-04-14 14:29:58 +0900

    ポーズ画面タイトルscene追加

---
+++ Gametaisyou/Assets/Script/Pause.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    private int minute;
+    private float second;
+    private int count;
+
+    private int pause;
+    private int MenuSelect = 0;
+
+    [SerializeField] GameObject PausePanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PauseCommand();
+    }
+
+    void PauseCommand()
+    {
+        {
+                if (Input.GetKeyDown("joystick button 7"))
+                {
+                    if (pause == 0)
+                    {
+                        PausePanel.SetActive(true);
+                        pause = 1;
+
+                        Time.timeScale = 0f;
+                    }
+                    else
+                    {
+                        PausePanel.SetActive(false);
+                        pause = 0;
+                        Time.timeScale = 1f;
+                    }
+                }
+                else
+                {
+                    ;
+                }
+        }
+    }
+}
