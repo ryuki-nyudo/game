1f98d16a5bf1898a022f411d7a9e3ecccf007c72
KBC12006040\kbc12006040 <ryuki-nyudou-20h@stu.kbc.ac.jp>
2022-05-13 10:06:36 +0900

    Reset

---
+++ Gametaisyou/Assets/Script/Controlitem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlitem : MonoBehaviour
+{
+
+    public float Loop_Speed;
+    public float down_length;
+    private float DeltaTimer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        DeltaTimer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        DeltaTimer += Time.deltaTime;
+
+        if (DeltaTimer > Loop_Speed)
+        {
+            transform.position += new Vector3(0, -down_length, 0);
+            DeltaTimer = 0f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D Coll)
+    {
+        Destroy(gameObject);
+    }
+}
